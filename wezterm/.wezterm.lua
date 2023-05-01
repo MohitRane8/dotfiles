c8e6c5bb561c7e265258156ddf3b1cfaec0f6b60
Mohit Rane <mohit.rane@colorado.edu>
2023-04-30 20:52:24 -0400

    Removed git branch display from zsh for better performance, changed zsh prompt

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,11 +17,31 @@ end
 -- config.color_scheme = 'Nature Suede (terminal.sexy)'
 config.color_scheme = 'Monokai Vivid'
 
+config.default_cursor_style = 'BlinkingBlock'
+
+-- Override colorscheme to change cursor color
+config.colors = {
+  -- Overrides the cell background color when the current cell is occupied by the
+  -- cursor and the cursor style is set to Block
+  cursor_bg = '#52ad70',
+  -- Overrides the text color when the current cell is occupied by the cursor
+  cursor_fg = 'black',
+  -- Specifies the border color of the cursor when the cursor style is set to Block,
+  -- or the color of the vertical or horizontal bar when the cursor style is set to
+  -- Bar or Underline.
+  cursor_border = '#52ad70',
+
+  -- the foreground color of selected text
+  selection_fg = 'black',
+  -- the background color of selected text
+  selection_bg = '#fffacd',
+}
+
 -- Default key bindings are listed here
 -- Disable this temporarily as sending Ctrl-W tries to close the wezterm
 -- This creates issue when using vim
 -- TODO: Change the leader key and learn the key bindings
-config.disable_default_key_bindings = true
+-- config.disable_default_key_bindings = true
 
 config.keys = {
   {
