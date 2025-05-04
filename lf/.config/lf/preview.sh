8e48abfedca8429b71310e1ecca04a29f309fb9a
Mohit Rane <mohit.rane@colorado.edu>
2025-05-04 12:34:13 -0400

    Added zsh vim plugin and added new WSL functions for lf

--- lf/.config/lf/preview.sh
+++ lf/.config/lf/preview.sh
@@ -10,5 +10,13 @@ case "$(file --dereference --brief --mime-type -- "$1")" in
     # *.rar) unrar l "$file" ;;
 	text/troff) man ./ "$1" | col -b ;;
 	text/* | */xml | application/json | application/x-ndjson) bat --terminal-width "$(($4-2))" -f "$1" ;;
+    application/octet-stream)
+        # Preview .bin files as hex dump with hexyl
+        if command -v hexyl > /dev/null 2>&1; then
+            hexyl --block-size=512 --length=36block "$1"
+        else
+            xxd -g 4 "$1" | head -n 100 | bat --style=plain --paging=never
+        fi
+        ;;
 esac
 exit 1
